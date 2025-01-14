d54ed92f741f2c4d53b5419e4f8d7356da8185c9
Mavl0nbek <mavlonbekisaqjonov222@gmail.com>
2025-01-14 16:20:12 +0500

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int out = 1;
+        for(int i=0; i<power; i++) {
+            out*=numberToPrint;
+        }
+        System.out.println(out);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for(int i=0; i<cathetusLength; i++) {
+            for(int k=0; k<cathetusLength-1-i; k++) {
+                System.out.print(" ");
+            }
+            for(int j=1+i; j>0; j--) {
+                System.out.print(j);
+            }
+            for(int j=2; j<2+i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
